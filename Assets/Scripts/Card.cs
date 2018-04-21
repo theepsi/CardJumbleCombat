007b61666a02102856389c2bdbe5736d2cb313b7
Ennio Casas Puglielli <theepsi@gmail.com>
2018-04-21 16:11:05 +0200

    Implement Cards logic

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CardType
+{
+    COMBO_INIT,
+    COMBO_MIDDLE,
+    COMBO_FINISHER
+}
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+public class Card : ScriptableObject {
+
+    public Sprite artwork;
+    public string description;
+
+    public int damage;
+
+    public CardType type;
+
+    public void Print()
+    {
+        Debug.Log(description + "-" + type);
+    }
+	
+}
---
+++ Assets/Scripts/CardDisplayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplayer : MonoBehaviour {
+
+    public Image cardCover;
+    public Image artwork;
+    public Text description;
+
+    public Card card;
+
+	void InitCard ()
+    {
+        switch (card.type)
+        {
+            case CardType.COMBO_INIT:
+                cardCover.sprite = Resources.Load<Sprite>("BaseCards/InitCard");
+                break;
+            case CardType.COMBO_MIDDLE:
+                cardCover.sprite = Resources.Load<Sprite>("BaseCards/MidleCard");
+                break;
+            case CardType.COMBO_FINISHER:
+                cardCover.sprite = Resources.Load<Sprite>("BaseCards/FinisherCard");
+                break;
+        }
+
+        artwork.sprite = card.artwork;
+        description.text = card.description;
+    }
+}
