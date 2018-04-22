264992efacee37b816a22b29056b69162feb84a2
Ennio Casas Puglielli <theepsi@gmail.com>
2018-04-22 14:45:24 +0200

    Prepare MainMenu with interactions

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+using UnityEngine.EventSystems;
+
+public class MainMenu : MonoBehaviour {
+
+    public EventTrigger[] elementsToDisable;
+
+    public Transform confirmationDialog;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+
+    public void ShowCredits()
+    {
+
+    }
+
+    public void Back()
+    {
+
+    }
+
+    public void ShowConfirmation()
+    {
+        ToggleButtons(false);
+        confirmationDialog.gameObject.SetActive(true);
+        confirmationDialog.DOLocalMoveY(0, .5f);//.DOJump(new Vector3(0f, 0f, 0f), 3f, 1, 1f);
+    }
+
+    public void DismissConfirmation()
+    {
+        confirmationDialog.DOLocalMoveY(-900f, .5f).OnComplete(() => confirmationDialog.gameObject.SetActive(false)); ;//confirmationDialog.GetComponent<RectTransform>().DOLocalJump(new Vector3(0f, -900f, 0f), 3f, 1, 1f).OnComplete(() => confirmationDialog.gameObject.SetActive(false));
+        ToggleButtons(true);
+    }
+
+    public void Quit(Transform data)
+    {
+        ShowConfirmation();
+
+        data.DOScale(new Vector3(1f, 1f, 1f), .5f);
+    }
+
+    public void ButtonEnter(Transform data)
+    {
+        data.DOScale(new Vector3(1.3f, 1.3f, 1f), .5f);
+    }
+
+    public void ButtonExit(Transform data)
+    {
+        data.DOScale(new Vector3(1f, 1f, 1f), .5f);
+    }
+
+    public void ConfirmationYes()
+    {
+        Application.Quit();
+    }
+
+    public void ConfirmationNo()
+    {
+        DismissConfirmation();
+    }
+
+    private void ToggleButtons(bool toggle)
+    {
+        for (int i = 0; i < elementsToDisable.Length; ++i)
+        {
+            elementsToDisable[i].enabled = toggle;
+        }
+    }
+}
